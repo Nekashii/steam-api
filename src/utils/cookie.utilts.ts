74b1360ab4280e50ff81e98efbca9b40244089f1
Rebecca Alitzel <nekashii@outlook.com>
2025-04-09 21:06:46 -0600

    fix: handle only-key headers

--- src/utils/cookie.utilts.ts
+++ src/utils/cookie.utilts.ts
@@ -4,14 +4,14 @@ export function getCookie(cookieString: string): Cookie {
   const cookie: Cookie = {}
   for (const part of cookieString.split(';')) {
     const [key, value] = part.trim().split('=')
-    const values = value.split(',')
-    cookie[key] = values.length > 1 ? values : value
+    const values = value?.split(',').map(value => value.trim())
+    cookie[key] = value && values.length > 1 ? values : value
   }
   return cookie
 }
 
 export function getCookieString(cookie: Cookie): string {
   return Object.entries(cookie)
-    .map(([key, value]) => `${key}=${Array.isArray(value) ? value.join(',') : value}`)
+    .map(([key, value]) => `${key}=${Array.isArray(value) ? value.join(', ') : value}`)
     .join('; ')
 }
