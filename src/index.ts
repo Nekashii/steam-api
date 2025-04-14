e7858e2c1cb4891d2cca6597aacae8352559963d
Rebecca Alitzel <nekashii@outlook.com>
2025-04-14 17:09:25 -0600

    feat: add authentication

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import { userGameStatsHandler } from './handlers/user-game-stats.handler'
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
+app.use(async ({ req, env: { AUTH_TOKEN }, text }, next) => {
+  if (req.header('Authorization') !== AUTH_TOKEN) return text('unauthenticated', 401)
+  await next()
+})
+
 app.get('/:prefix/:userId/stats/:appId', userGameStatsHandler)
 
 export default app
